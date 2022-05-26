75e65d80d9ba3f34a497ae97ac9b428edef42f47
Maksim Marchenkov <marchenkovby@gmail.com>
2022-05-26 19:48:18 +0300

    Update index.php

--- public_html/index.php
+++ public_html/index.php
@@ -3,12 +3,9 @@
 if (PHP_MAJOR_VERSION <8) {
     die('PHP version required >= 8');
 }
-else {
-    echo 'PHP version OK!';
-}
 
 require_once dirname(__DIR__) . '/config/init.php';
 
 new \aruka\App();
 
-vardump(\aruka\App:$app->getProperties());
+var_dump(\aruka\App:$app->getProperties());
