699fa06d6ab19e533c1849debdad72d01629956d
Maksim Marchenkov <marchenkovby@gmail.com>
2022-05-26 10:54:36 +0300

    Update init.php

--- config/init.php
+++ config/init.php
@@ -2,3 +2,19 @@
 
 define("DEBUG", 1);
 define("ROOT", dirname(__DIR__));
+define("WWW", ROOT . '/public_html');
+define("APP", ROOT . '/app');
+define("CORE", ROOT . '/vendor/aruka');
+define("HELPERS", ROOT . '/vendor/aruka/helpers');
+define("CACHE", ROOT . '/tmp/cache');
+define("LOGS", ROOT . '/tmp/logs');
+define("CONFIG", ROOT . '/config');
+define("LAYOUT", 'aruka');
+define("PATH", 'https://test.howto.by');
+//define("PATH", 'http://aruka.loc');
+define("ADMIN", PATH . '/admin');
+// ?
+define("NO_IMAGE", PATH . 'upload/no_image.jpg');
+
+
+require_once ROOT . '/vendor/autoload.php';
