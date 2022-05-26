0a3ca0c8ada6c3f7988cd9e689ad5a75a24435e6
Maksim Marchenkov <marchenkovby@gmail.com>
2022-05-26 19:45:45 +0300

    Update App.php

--- vendor/aruka/App.php
+++ vendor/aruka/App.php
@@ -9,10 +9,17 @@ class App
     public function __construct()
     {
         self::$app = Registry::getInstance();
+        $this->getParams();
     }
 
     protected function getParams()
     {
-        
+        $params = require_once CONFIG . /'params.php';
+        if (!empty($params) {
+            foreach ($params as $k => $v) {
+                self::$app->setProperty($k, $v);
+            }
+
+        })
     }
 }
