f5b09f2007963fdf597453576739b45461b1cdf1
Maksim Marchenkov <marchenkovby@gmail.com>
2022-05-26 16:52:53 +0300

    Update TSingleton.php

--- vendor/aruka/TSingleton.php
+++ vendor/aruka/TSingleton.php
@@ -6,10 +6,11 @@ trait TSingleton
 {
     private static ?self $instance = null;
     
-    private function __construct(){}
+    // Protect from create through new TSingleton
+    private function __construct () {}
 
     public static function getInstance(): static
     {
         return static::$instance ?? static::$instance = new static();
     }
-    
+}
