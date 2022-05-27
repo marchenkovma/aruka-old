f2c462fc5ff677c9a0536de06e8d2090b2e32a5b
Maksim Marchenkov <marchenkovby@gmail.com>
2022-05-27 18:26:20 +0300

    Update index.php

--- public_html/index.php
+++ public_html/index.php
@@ -10,6 +10,5 @@ new \aruka\App();
 
 echo \aruka\App::$app->getProperty('pagination');
 echo '<br>';
-var_dump(\aruka\App::$app->getProperties());
-
 \aruka\App::$app->getProperty('test', 'TEST');
+var_dump(\aruka\App::$app->getProperties());
