24d3f82ecdcc930ad723bbf7439c8e6847e16991
Maksim Marchenkov <marchenkovby@gmail.com>
2022-05-27 18:23:00 +0300

    Update index.php

--- public_html/index.php
+++ public_html/index.php
@@ -8,4 +8,6 @@ require_once dirname(__DIR__) . '/config/init.php';
 
 new \aruka\App();
 
+echo \aruka\App:$app->getProperty('pagination');
+echo '<br>';
 var_dump(\aruka\App::$app->getProperties());
