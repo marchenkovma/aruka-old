dbf42cd470686919554b6c8b7b036a4d51382689
Maksim Marchenkov <marchenkovby@gmail.com>
2022-05-28 16:03:35 +0300

    Update router table

--- config/routes.php
+++ config/routes.php
@@ -5,3 +5,4 @@ use aruka\Router;
 // ^ - begin line
 // & - end line
 Router::add('^$', ['controller' => 'Main', 'action' => 'index']);
+Router::add('^(?P<controller>[a-z-]+)/(?P<action>[a-z-]+)/?$');
