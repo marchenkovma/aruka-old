7e4a6fc7b57568165de6a72b53d88c2b80415dd4
Maksim Marchenkov <marchenkovby@gmail.com>
2022-05-28 15:36:49 +0300

    Update Router.php

---
+++ vendor/aruka/Router.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace aruka;
+
+class Router
+{
+    protected static array $routes = [];
+    protected static array $route = [];
+
+    public static function add($regexp, $route = [])
+    {
+        self::$routes[$regexp] = $route;
+    }
+
+    public static function getRoutes(): array
+    {
+        return self::$routes;
+    }
+
+    public static function getRoute(): array
+    {
+        return self::$route;
+    }   
+}
