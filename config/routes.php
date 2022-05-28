26c7fe3460de2efc40d03e5b67fd1d1970ddaefa
Maksim Marchenkov <marchenkovby@gmail.com>
2022-05-28 16:17:26 +0300

    Update router table

--- config/routes.php
+++ config/routes.php
@@ -2,7 +2,14 @@
 
 use aruka\Router;
 
+Router::add('^admin/?$', ['controller' => 'Main', 'action' => 'index', 'admin_prefix' => 'admin']);
+
 // ^ - begin line
 // & - end line
 Router::add('^$', ['controller' => 'Main', 'action' => 'index']);
+
+// ?P<controller> - array with key сontroller
+// a-z - all alphabet lettes
+// - Underscore
+// + - at least 1 character
 Router::add('^(?P<controller>[a-z-]+)/(?P<action>[a-z-]+)/?$');
