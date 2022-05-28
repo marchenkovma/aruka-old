c4f2a3a189f7007ce1183c808ed4f4bb832ef2a0
Maksim Marchenkov <marchenkovby@gmail.com>
2022-05-28 15:06:11 +0300

    Update init.php

--- config/init.php
+++ config/init.php
@@ -10,7 +10,8 @@ define("CACHE", ROOT . '/tmp/cache');
 define("LOGS", ROOT . '/tmp/logs');
 define("CONFIG", ROOT . '/config');
 define("LAYOUT", 'aruka');
-define("PATH", 'https://test.howto.by');
+define("PROTOCOL", 'https://')
+define("PATH", PROTOCOL . 'test.howto.by');
 //define("PATH", 'http://aruka.loc');
 define("ADMIN", PATH . '/admin');
 // ?
