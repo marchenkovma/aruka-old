5b39fad3cb14168eb67278ac0aed74b2878c5cd6
Maksim Marchenkov <marchenkovby@gmail.com>
2022-05-28 15:28:17 +0300

    Update index.php

--- public_html/index.php
+++ public_html/index.php
@@ -6,6 +6,7 @@ if (PHP_MAJOR_VERSION < 8) {
 
 require_once dirname(__DIR__) . '/config/init.php';
 require_once HELPERS . '/functions.php';
+require_once CONFIG . '/routes.php';
 
 new \aruka\App();
 
