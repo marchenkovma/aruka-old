0f43a2badf771d56fbd4fef9b99544345ca82ef0
Maksim <marchenkovby@gmail.com>
2022-05-29 14:13:04 +0300

    Update Router.php

--- vendor/aruka/Router.php
+++ vendor/aruka/Router.php
@@ -36,9 +36,44 @@ class Router
         foreach (self::$routes as $pattern => $route) {
             // #{$pattern}#i - case-independent
             if (preg_match("#{$pattern}#", $url, $matches)) {
+                foreach ($matches as $k => $v) {
+                    if (is_string($k)) {
+                        $route [$k] = $v;
+                    }
+                }
+                if (empty($route['action'])) {
+                    $route['action'] = 'index';
+                }
+                if (!isset($route['admin_prefix'])) {
+                    $route['admin_prefix'] = '';
+                }
+                else {
+                    $route['admin_prefix'] = '\\';
+                }
+                debug($route);
+                $route['controller'] = self::upperCamelCase($route['controller']);
+                debug($route);
                 return true;
             }
         }
         return false;
     }
+
+    // new-product => NewProduct
+    protected static function upperCamelCase($name): string
+    {
+        // new-product => new product
+        //$name= str_replace('-', '', $name);
+        // new product => New Product
+        //$name = ucwords($name);
+        // New prodcut => NewProduct
+        //$name = str_replace(' ', '', $name);
+
+        return str_replace(' ', '', ucwords(str_replace('-', '', $name)));
+    }
+    // PSR 4?
+    // CamelCase - Class
+    // new-product => NewProduct
+    // camelCase - Method
+
 }
