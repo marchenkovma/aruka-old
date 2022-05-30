ce93433955532128b59ec8d94bf0e7be6fc4bdd6
Maksim Marchenkov <marchenkovby@gmail.com>
2022-05-30 13:33:42 +0300

    Update base controller

--- vendor/aruka/Controller.php
+++ vendor/aruka/Controller.php
@@ -4,8 +4,22 @@ namespace aruka;
 
 abstract class Controller
 {
+    // New PHP8
+    public array $data = [];
+    public array $meta = ['title' => '', 'description' => '', 'keywords' => ''];
+    public false|string $layout = '';
+    public string $view = '';
+    public string $model = '';
+
+
     public function __construct(public $route = [])
     {
          
+    }
+
+    public function getModel()
+    {
+        $model = 'app\models\\' . $this->route['admin_prefix'] . $this->route['controller'];
+        $this->model = $model;
     }
 }
