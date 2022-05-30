12a06f4427dd1594abcf584d75ff0a8c7ce702f6
Maksim Marchenkov <marchenkovby@gmail.com>
2022-05-30 19:15:19 +0300

    Update base controller

--- vendor/aruka/Controller.php
+++ vendor/aruka/Controller.php
@@ -12,7 +12,7 @@ abstract class Controller
 
     public function __construct(public $route = [])
     {
-         
+        debug($this->route)
     }
 
     public function getModel()
@@ -26,6 +26,7 @@ abstract class Controller
     public function getView()
     {
         $this->view = $this->view ?: $this->route['action'];
+        (new View($this->route, $this->layout, $this->view, $this->meta))->render($this->data);
     }
     
     public function set($data)
--- vendor/aruka/Router.php
+++ vendor/aruka/Router.php
@@ -49,6 +49,9 @@ class Router
                 $action = self::lowerCamelCase(self::$route['action'] . 'Action');
                 if (method_exists($controllerObject, $action)) {
                     $controllerObject->$action();
+
+                    $controllerObject->getView();
+
                 } else {
                     throw new \Exception ("Action {$controller}::{$action} not found", 404); 
                 }
