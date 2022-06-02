0b920f5597382657cebba15e88ef92d257ad56d3
Maksim Marchenkov <marchenkovby@gmail.com>
2022-06-02 09:58:03 +0300

    Update MainController.php

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -6,7 +6,7 @@ use aruka\Controller;
 
 class MainController extends Controller
 {
-    public $layout = 'test2';
+    public false|string $layout = 'test2';
 
     public function indexAction()
     {
