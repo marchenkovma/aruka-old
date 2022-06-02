8a56399a5d9c2acd6195b817d804c1bf383ae25c
Maksim <marchenkovby@gmail.com>
2022-06-02 22:54:20 +0300

    Add function htmlspecialchars

--- vendor/aruka/View.php
+++ vendor/aruka/View.php
@@ -48,9 +48,9 @@ class View {
 
     public function getMeta()
     {
-        $out = '<title>' . $this->meta['title'] . '</title>' . PHP_EOL;
-        $out .= '<meta name="description" content="' . $this->meta['description'] . '">' . PHP_EOL;
-        $out .= '<meta name="keywords" content="' . $this->meta['keywords'] .'">' . PHP_EOL;
+        $out = '<title>' . h($this->meta['title']) . '</title>' . PHP_EOL;
+        $out .= '<meta name="description" content="' . h($this->meta['description']) . '">' . PHP_EOL;
+        $out .= '<meta name="keywords" content="' . h($this->meta['keywords']) .'">' . PHP_EOL;
         return $out;
     }
 }
