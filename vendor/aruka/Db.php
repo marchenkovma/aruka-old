6eb21149909f58d78f7fe38e9a995ea50bbcb790
Maksim Marchenkov <marchenkovby@gmail.com>
2022-06-04 14:47:57 +0300

    Update Db.php

--- vendor/aruka/Db.php
+++ vendor/aruka/Db.php
@@ -8,7 +8,15 @@ class Db {
 
     private function __construct()
     {
-        $db = require_once CONFIG . '/config_db.php'
-    }
+        $db = require_once CONFIG . '/config_db.php';
+        R::setup($db['dsn'], $db['user'], $db['password']);
+        if (R::testConnection()) {
+            throw new \Exception('No connection for db', 500);
+        }
+        R::freeze(true);
+        if (DEBUG) {
+            R::debug(true, 3);
+        }
 
+    }
 }
