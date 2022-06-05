6d3a1f19aa74d18d7e81f0fd3bc6b28dcea856ec
Maksim <marchenkovby@gmail.com>
2022-06-05 15:10:38 +0300

    Update MainController.php

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -3,12 +3,15 @@
 namespace app\controllers;
 
 use aruka\Controller;
+use ReadBeanPHP\R;
 
 class MainController extends Controller
 {
 
     public function indexAction()
     {
+        $name = R::findAll('name');
+        debug($name);
         $this->setMeta('Main page', 'Description', 'Keywords' );
     }
 }
