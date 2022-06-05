563d9485c86dd38d0033d9ea760f3dcbe8a00021
Maksim <marchenkovby@gmail.com>
2022-06-05 15:51:50 +0300

    Update index.php

--- app/view/Main/index.php
+++ app/view/Main/index.php
@@ -1,3 +1,7 @@
 <h1>Hello, Main/index</h1>
 
-<?php debug($name); ?>
+<?php if (!empty($name)): ?>
+    <?php foreach ($name as $name): ?>
+        <?= $name->id ?> => <?= $name->name ?><br>
+    <?php endforeach; ?>
+<?php endifl ?>
