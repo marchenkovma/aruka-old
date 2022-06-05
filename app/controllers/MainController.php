8d9753b30af6af591b0b42a42922fa3358f3e492
Maksim <marchenkovby@gmail.com>
2022-06-05 16:13:39 +0300

    Update MainController.php

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -3,6 +3,7 @@
 namespace app\controllers;
 
 use aruka\Controller;
+use RedBeanPHP\R;
 
 /** @property Main $model */
 class MainController extends Controller
@@ -11,6 +12,7 @@ class MainController extends Controller
     public function indexAction()
     {
         $name = $this->model->getName();
+        $one_name = R::getRow('SELECT * FROM name WHERE id = 2')
         $this->setMeta('Main page', 'Description', 'Keywords' );
         $this->set(compact('name'));
     }
